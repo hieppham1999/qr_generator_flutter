f924b991b220ce96ebf0b268de878451b69cdf10
HiepPT <hieppt1001@gmail.com>
2025-06-11 16:51:19 +0700

    add convex app bar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_generator_flutter/core/languages.dart';
 
@@ -13,43 +14,32 @@ class AppBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      // Visually indicates which regular tab is active
+
+    print(Theme.of(context).bottomAppBarTheme.color);
+
+    return ConvexAppBar(
+      initialActiveIndex: 1,
       onTap: onItemTapped,
-      type: BottomNavigationBarType.fixed,
+      style: TabStyle.fixedCircle,
+      activeColor: Colors.amber[800],
+      color: Colors.white,
+      backgroundColor: Theme.of(context).bottomAppBarTheme.color,
       // Ensures all items are visible and labels shown
-      selectedItemColor: Colors.amber[800],
-      // Optional: Color for selected item
-      unselectedItemColor: Colors.grey,
-      // Optional: Color for unselected items
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: Languages.translate.homeTab,
+      items: [
+        TabItem(
+          icon: Icons.home,
+          title: Languages.translate.homeTab,
         ),
         // This is our "QR Scan" button in the middle.
         // It doesn't change _currentIndex in the same way if it's purely an action button.
-        BottomNavigationBarItem(
-          icon: Container(
-            // Wrap with Container for custom styling if needed
-            padding: const EdgeInsets.all(8.0),
-            // Add padding if you want it larger
-            decoration: BoxDecoration(
-              // color: Colors.blue, // Optional: different background for emphasis
-              // shape: BoxShape.circle, // Optional: make it circular
-            ),
-            child: const Icon(
-              Icons.qr_code_scanner,
-              size: 30,
-            ), // Slightly larger icon
-          ),
-          label:
+        TabItem(
+          icon: Icons.qr_code_scanner,
+          title:
               'Scan QR', // Label is optional, can be empty string for just icon
         ),
-        const BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'Settings',
+        const TabItem(
+          icon: Icons.settings,
+          title: 'Settings',
         ),
       ],
     );
